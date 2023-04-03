daf733c65531bc9767fa8207e6105f6add112e77
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-04-03 05:40:44 -0300

    Est.
    ## Tópicos Básicos Module Systems (
    Navegando em Diretórios
    Tópicos Básicos Props System
    Desestruturação de Objetos (Intro)

--- first/src/App.js
+++ first/src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard title="Alexa" handle="@alexa99"/>
+      <ProfileCard title="Cortana" handle="@cortana99"/>
+      <ProfileCard title="Siri" handle="@siri99"/>
     </div>
   );
 }
--- first/src/ProfileCard.js
+++ first/src/ProfileCard.js
@@ -1,5 +1,13 @@
-function ProfileCard() {
-  return <div>Profile Card</div>;
+function ProfileCard({title, handle}) {
+  //const title = props.title
+  //const handle = props.handle
+  //const {title, handle} = props;
+  return (
+    <div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  )
 }
 
 export default ProfileCard;
