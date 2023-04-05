53964ce5224b75e6f5f9d36a7b460a96722e81bf
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-04-05 11:06:21 -0300

    useSTate

--- second/src/App.js
+++ second/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()]); //Não usar PUSH, pq o PUSH, modifica o piece of state
+  };
+
+  const renderedAnimals = animals.map((qualquerPalavra, index) => {
+    return <AnimalShow type={qualquerPalavra} key={index} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 }
