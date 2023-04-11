6598a4506e9134b63711f7b6154f5fe073653891
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-04-11 19:34:24 -0300

    Handle Form
    -Created useStrate for books added.
    - Created Handler for Books added
    - Created userState for bookCreat component
    - Created HandleSubmit
    - Created HandleChange

---
+++ handleforms/src/components/BookCreate.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState( ''); //1. Ao criar o Create, é preciso setar um useState no input, pois cada vez será modificado.
+
+  const handleChange = (event) => {
+    //2.1 Criar um Handle Event para lidar com o evento de criação, event será o nome adicionado.
+    setTitle(event.target.value);
+  };
+  // 2.2 Passa os argumentos para os paramentros no input, o titulo do useState e o handleChange.
+
+  //3. Criar o botão de submite que vai trigar o evento de submeter do formulário, vai ativar o event handler, logo, vai chamar o onCreate e vai passar o Title desejado para a função.
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); //3.1 Essa linha vai impedir que o browser atualize automático, antes de rodar o resto do código.
+    onCreate(title); //3.2 Aqui chama a função citada no step 3.
+    setTitle('')
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input value={title} onChange={handleChange} />
+        <button>Create!</button>
+      </form>
+    </div>
+  );
+}
+
+export default BookCreate;
