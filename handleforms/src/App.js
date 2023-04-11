5ed4fd3f83c263c9cf960804e96581e93549b42e
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-04-11 20:01:24 -0300

    handleBooks
    - Class explaning WHY WITH .PUSH React dont renderize.

--- handleforms/src/App.js
+++ handleforms/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [books, setBooks] = useState([]); //1,2. Os livros serão adicionados no array pelo BookCreate.
 
   const HandleCreateBook = (title) => { //2,1. Create the Event Handler.
-    console.log('Need to add this book:', title)
+
   }
 
   return <div><BookCreate onCreate={HandleCreateBook}/></div>; //2,3. Display the Prop CreateBook on App, can be any name (onOrange)
