e067dffb75c1eb8967a83148873b8fa966e0763c
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-04-12 16:27:30 -0300

    HandleForms
    Book Create implemented
    Book List implemented
    Book Show implemented
    Styled

--- handleforms/src/App.js
+++ handleforms/src/App.js
@@ -1,14 +1,22 @@
 import { useState } from "react"; //1,1.Determinar o useState
-import BookCreate from "./components/BookCreate"; //2,2. Import the Component.
+import BookCreate from "./components/BookCreate"; //2,2. Import the create Component.
+import BookList from "./components/BookList"; //4. Import the BookList Component
 
 function App() {
   const [books, setBooks] = useState([]); //1,2. Os livros serão adicionados no array pelo BookCreate.
 
-  const HandleCreateBook = (title) => { //2,1. Create the Event Handler.
+  const CreateBook = (title) => { //2,1. Create the Event Handler.
+    const updatedBooks = [...books, { id: Math.round(Math.random() * 9999), title }]; //4. Adicione dentro do add function, a função para gerar um ID aleatório para seu livro, esse método não gera ID único, mas é o suficiente para a aplicação.
+    setBooks(updatedBooks); //4.1 . Create the ADD function
+  };
 
-  }
-
-  return <div><BookCreate onCreate={HandleCreateBook}/></div>; //2,3. Display the Prop CreateBook on App, can be any name (onOrange)
+  return (
+    <div className="app">
+      <BookList books= {books}/> 
+      <BookCreate onCreate={CreateBook} />
+    </div>
+  ); //2,3. Display the Prop CreateBook on App, can be any name (onOrange)
+  //5.1 Chame o componente BookList para o JSX, com a prop Books.
 }
 
 export default App;
--- handleforms/src/components/BookShow.js
+++ handleforms/src/components/BookShow.js
@@ -1,5 +1,5 @@
-function BookShow() {
-    return <div>BookShow</div>
+function BookShow({book}) { //5. Recebe a prop book
+    return <div className="book-show">{book.title}</div> //5.2Retorne o JSX com o titulo do livro
     }
 
     export default BookShow
