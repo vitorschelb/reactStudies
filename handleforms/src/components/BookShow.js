1a34421f36223eec657008b1b3bb4fa1dcf47f19
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-04-13 21:42:59 -0300

    BookEdit
    Created the edit state and the button

--- handleforms/src/components/BookShow.js
+++ handleforms/src/components/BookShow.js
@@ -1,15 +1,35 @@
-function BookShow({book, onDelete}) { //5. Recebe a prop book //6.3 Veja que é passada a PROP onDelte aqui, a prop onDelete, remete a função deleteBookById!
-    const handleClick = () => {
-        onDelete(book.id)
-    }
-    return <div className="book-show">
-        {book.title} 
-        <div className="actions">
-            <button className="delete" onClick={handleClick}>
-                Delete
-            </button>
-        </div>
-        </div> //5.2Retorne o JSX com o titulo do livro
-    } //6.4 Crie o JSX com os elementos do botão e a div que irá o abrigar. Chame a função onclick do botão, porém ele não deve receber a prop onDelete, por que quando se chama onDelete é esperado que passemps o ID do livro que é para ser deletado. É preciso criar uma função handleClick e chama-la, e ENTÃO dentro dela passar o id do livro que quer ser deletado
+import { useState } from "react";
+import BookEdit from "./BookEdit";
 
-    export default BookShow
+function BookShow({ book, onDelete }) {
+  //5. Recebe a prop book //6.3 Veja que é passada a PROP onDelte aqui, a prop onDelete, remete a função deleteBookById!
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
+    onDelete(book.id);
+  };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
+  let content = <h3>{book.title}</h3>
+  if(showEdit) {
+    content = <BookEdit />
+  }
+  return (
+    <div className="book-show">
+      <div>{content}</div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditClick}>
+          edit
+        </button>
+        <button className="delete" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div> //5.2Retorne o JSX com o titulo do livro
+  );
+} //6.4 Crie o JSX com os elementos do botão e a div que irá o abrigar. Chame a função onclick do botão, porém ele não deve receber a prop onDelete, por que quando se chama onDelete é esperado que passemps o ID do livro que é para ser deletado. É preciso criar uma função handleClick e chama-la, e ENTÃO dentro dela passar o id do livro que quer ser deletado
+
+export default BookShow;
