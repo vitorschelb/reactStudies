eac0b9b038da7dfd2c45e3af800223523409004c
vitorschelb <116350364+vitorschelb@users.noreply.github.com>
2023-04-20 18:59:23 -0300

    comps
    comp accordion
    handleclick studies

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,31 +1,12 @@
-import Button from "../components/Button";
-import { GoBell } from "react-icons/go";
+import Accordion from "./components/Accordion";
 
 function App() {
-  return (
-    <div>
-      <div>
-        <Button secondary className="mb-5">
-          <GoBell />
-          Primary
-        </Button>
-      </div>
-      <div>
-        <Button secondary>Secondary</Button>
-      </div>
-      <div>
-        <Button success rounded>
-          Success
-        </Button>
-      </div>
-      <div>
-        <Button warning>Warning</Button>
-      </div>
-      <div>
-        <Button danger>Danger</Button>
-      </div>
-    </div>
-  );
+  const items = [
+    { label: "Can I use React on a project?", content: "You can you React on any project you want.", id: 1 },
+    { label: "Can I use JavaScript on a project?", content: "You can you JavaScript on any project you want.", id: 2 },
+    { label: "Can I use CSS on a project?", content: "You can you CSS on any project you want.", id:3 },
+  ];
+  return <Accordion items = {items}/>;
 }
 
 export default App;
